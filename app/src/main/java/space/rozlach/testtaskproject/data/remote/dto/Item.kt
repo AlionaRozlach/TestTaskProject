164b9318f8e01652f33339a62d8b648ab94acc09
ar617wx <alona.rozlach@student.tuke.sk>
2024-03-09 17:29:38 +0100

    --added dto Item

---
+++ app/src/main/java/space/rozlach/testtaskproject/data/remote/dto/Item.kt
@@ -0,0 +1,12 @@
+package space.rozlach.testtaskproject.data.remote.dto
+
+data class Item(
+    val popisk: String,
+    val schkz: String,
+    val schkzText: String,
+    val typk: String,
+    val begda: String,
+    val endda: String,
+    val exported: String,
+    val filename: String,
+)
