2dd6c7cd44419bb8aed1e1cb03d0e5ffca258d5e
ar617wx <alona.rozlach@student.tuke.sk>
2024-03-12 23:28:59 +0100

    --edited ItemDto

--- app/src/main/java/space/rozlach/testtaskproject/data/remote/dto/ItemDto.kt
+++ app/src/main/java/space/rozlach/testtaskproject/data/remote/dto/ItemDto.kt
@@ -2,42 +2,27 @@ package space.rozlach.testtaskproject.data.remote.dto
 
 import space.rozlach.testtaskproject.domain.model.Item
 
-//data class ItemDto(
-//    val popisk: String
-//)
-//{
-//    fun toItem(): Item {
-//        return Item(
-//            popisk = popisk,
-////            schkz = schkz,
-////            schkzText = schkzText,
-////            typk = typk,
-////            begda = begda,
-////            endda = endda,
-////            exported = exported,
-////            filename = filename
-//        )
-//    }
-//}
-
-
 data class ItemDto(
-    val popisk: String = ""
+    val popisk: String = "",
+    val schkz: String = "",
+    val schkz_text: String = "",
+    val typk: String = "",
+    val begda: String = "",
+    val endda: String = "",
+    val exported: String = "",
+    val filename: String = ""
 ) {
-    // Add a no-argument constructor if required by Firebase
-    constructor() : this("")
-
     fun toItem(): Item {
         return Item(
-            popisk = popisk
+            popisk = popisk,
             // Uncomment the lines below if you want to include additional properties
-            // schkz = schkz,
-            // schkzText = schkzText,
-            // typk = typk,
-            // begda = begda,
-            // endda = endda,
-            // exported = exported,
-            // filename = filename
+            schkz = schkz,
+            schkz_text = schkz_text,
+            typk = typk,
+            begda = begda,
+            endda = endda,
+            exported = exported,
+            filename = filename
         )
     }
 }
--- app/src/main/java/space/rozlach/testtaskproject/domain/model/Item.kt
+++ app/src/main/java/space/rozlach/testtaskproject/domain/model/Item.kt
@@ -2,11 +2,11 @@ package space.rozlach.testtaskproject.domain.model
 
 data class Item(
     val popisk: String,
-//    val schkz: String,
-//    val schkzText: String,
-//    val typk: String,
-//    val begda: String,
-//    val endda: String,
-//    val exported: String,
-//    val filename: String
+    val schkz: String,
+    val schkz_text: String,
+    val typk: String,
+    val begda: String,
+    val endda: String,
+    val exported: String,
+    val filename: String
 )
