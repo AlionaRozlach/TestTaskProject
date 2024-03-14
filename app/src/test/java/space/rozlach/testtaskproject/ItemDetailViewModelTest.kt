115b51ce35f3cf172960550b4eb6b318fab99d66
Aliona Rozlach <alona.rozlach@gmail.com>
2024-03-14 17:58:04 +0100

    --added ItemDetailViewModel test

---
+++ app/src/test/java/space/rozlach/testtaskproject/ItemDetailViewModelTest.kt
@@ -0,0 +1,84 @@
+package space.rozlach.testtaskproject
+
+import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import space.rozlach.testtaskproject.core.Resource
+import space.rozlach.testtaskproject.data.remote.dto.ItemDetailDto
+import space.rozlach.testtaskproject.domain.use_case.get_item.GetItemUseCase
+import space.rozlach.testtaskproject.presentation.items_detail.ItemDetailState
+import space.rozlach.testtaskproject.presentation.items_detail.ItemDetailViewModel
+import kotlin.test.assertEquals
+
+class ItemDetailViewModelTest {
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    private lateinit var getItemUseCase: GetItemUseCase
+    private lateinit var savedStateHandle: SavedStateHandle
+
+    private lateinit var viewModel: ItemDetailViewModel
+
+    @Before
+    fun setUp() {
+        getItemUseCase = mock(GetItemUseCase::class.java)
+        savedStateHandle = mock(SavedStateHandle::class.java)
+        viewModel = ItemDetailViewModel(getItemUseCase, savedStateHandle)
+    }
+
+    @Test
+    fun `test loading state`() {
+        testScope.runBlockingTest {
+            val popisk = "example_popisk"
+            `when`(savedStateHandle.get<String>("popisk")).thenReturn(popisk)
+            `when`(getItemUseCase(popisk)).thenReturn(flow {
+                emit(Resource.Loading())
+            })
+
+            viewModel.getItem(popisk)
+
+            val expectedState = ItemDetailState(isLoading = true)
+            assertEquals(expectedState, viewModel.state.value)
+        }
+    }
+
+    @Test
+    fun `test success state`() {
+        testScope.runBlockingTest {
+            val popisk = "example_popisk"
+            val item = ItemDetailDto("1", popisk).toItemDetail()
+            `when`(savedStateHandle.get<String>("popisk")).thenReturn(popisk)
+            `when`(getItemUseCase(popisk)).thenReturn(flow {
+                emit(Resource.Success(item))
+            })
+
+            viewModel.getItem(popisk)
+
+            val expectedState = ItemDetailState(items = item)
+            assertEquals(expectedState, viewModel.state.value)
+        }
+    }
+
+    @Test
+    fun `test error state`() {
+        testScope.runBlockingTest {
+            val popisk = "example_popisk"
+            val errorMessage = "Failed to load item"
+            `when`(savedStateHandle.get<String>("popisk")).thenReturn(popisk)
+            `when`(getItemUseCase(popisk)).thenReturn(flow {
+                emit(Resource.Error(errorMessage))
+            })
+
+            viewModel.getItem(popisk)
+
+            val expectedState = ItemDetailState(error = errorMessage)
+            assertEquals(expectedState, viewModel.state.value)
+        }
+    }
+}
